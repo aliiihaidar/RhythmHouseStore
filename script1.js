de738aeedc93eba1f60d00e3190bf8a730989b9d
aliiihaidar <49180928+aliiihaidar@users.noreply.github.com>
2026-04-09 22:43:07 +0500

    Basic project

---
+++ script1.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(function(){
+	var navbar = $('.navbar');
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() <= 40){
+			navbar.removeClass('navbar-scroll');
+		} else {
+			navbar.addClass('navbar-scroll');
+		}
+	});
+});
